57cf40de0eb94ae0671837b1f94962a97656ce13
Mathis Rosenhauer <rosenhauer@dkrz.de>
2015-10-26 13:10:25 +0100

    Chage license to GPL2 for compatibility with USB Host library.

--- Arduino/THR_Footswitch/THR_Footswitch.ino
+++ Arduino/THR_Footswitch/THR_Footswitch.ino
@@ -1,40 +1,19 @@
 /*
- * Copyright 2015
- * Mathis Rosenhauer
+ * Arduino program for sending preset data to a Yamaha THR10 guitar
+ * amplifier.  Copyright (C) 2015 Mathis Rosenhauer
  *
- * All rights reserved.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above
- *    copyright notice, this list of conditions and the following
- *    disclaimer in the documentation and/or other materials provided
- *    with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*
-  Sketch for an Arduino Uno (other types may work as well) with an USB
-  host shield and SD card adapter. A Yamaha THR10/5 is connected to
-  the USB host shield. The SD card holds a patch file as used with the
-  THR editor. Patches are read from the SD card and sent to the
-  THR. Two switches are used to navigate through the patch file.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, If not, see <http://www.gnu.org/licenses/>
 */
 
 #include <Bounce2.h>
